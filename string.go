932241312248fbe818ce9a50ce5a2d30ab9eb3d0
muhammadyusronn <muhammadyusron733@gmail.com>
2021-07-24 19:22:55 +0700

    Menambahkan menghitung string

--- string.go
+++ string.go
@@ -5,4 +5,8 @@ import "fmt"
 func main() {
 	fmt.Println("Yusron")
 	fmt.Println("Muhammad Yusron Hartoyo")
+	// Menghitung jumlah string
+	fmt.Println(len("Yusron"))
+	// Mengambil karakter berdasarkan posisi yang ditentukan
+	fmt.Println("CyberlabSWJ"[1])
 }
